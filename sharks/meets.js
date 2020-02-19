a731229c3e03bf609ed567526b04ba32a50f3dbf
Zibing Zhang <zzhang2022@gmail.com>
2020-02-19 16:18:47 -0500

    Added functionality to add and sub buttons
    Add and sub buttons now add and subtract rows from their respective
    section
    Added classes to buttons so that they were easier to locate
    By default heat inputs are filled in with 'H?L?' while swimmer inputs
    are left blank

--- sharks/meets.js
+++ sharks/meets.js
@@ -44,4 +44,64 @@ addOtherMeets([
   ['Mile Swim', ],
   ['B Regionals', ],
   ['A Regionals', ]
-])
+]);
+
+/* =========================================== */
+
+function addRowToSection(sectionId) {
+  function newHeatInput() {
+    const heat = document.createElement('input');
+    heat.className = 'meet-section-heats-input';
+    heat.spellcheck = 'false';
+    heat.value = 'H?L?';
+    return heat;
+  }
+
+  function newSwimmerInput() {
+    const swimmer = document.createElement('input');
+    swimmer.className = 'meet-section-swimmers-input';
+    swimmer.spellcheck = 'false';
+    return swimmer;
+  }
+
+  const heatSections = document.querySelectorAll(`#${sectionId} .meet-section-heats`);
+  const swimmerSections = document.querySelectorAll(`#${sectionId} .meet-section-swimmers`);
+
+  heatSections.forEach(heatSection => {
+    heatSection.appendChild(newHeatInput());
+  });
+  swimmerSections.forEach(swimmerSection => {
+    swimmerSection.appendChild(newSwimmerInput());
+  });
+}
+
+function subtractRowFromSection(sectionId) {
+  const heatSections = document.querySelectorAll(`#${sectionId} .meet-section-heats`);
+  heatSections.forEach(heatSection => {
+    heatSection.removeChild(heatSection.lastElementChild);
+  });
+
+  const swimmerSections = document.querySelectorAll(`#${sectionId} .meet-section-swimmers`);
+  swimmerSections.forEach(swimmerSection => {
+    swimmerSection.removeChild(swimmerSection.lastElementChild);
+  });
+}
+
+function listenSectionButton(sectionId, buttonClass, onClickFunction) {
+  document.querySelector(`#${sectionId} .${buttonClass}`).onclick = onClickFunction;
+}
+
+function linkAllButtonsAgeSection(sectionId) {
+  listenSectionButton(sectionId, 'add-row', () => addRowToSection(sectionId));
+  listenSectionButton(sectionId, 'sub-row', () => subtractRowFromSection(sectionId));
+}
+
+[
+  'section-8-under',
+  'section-9-10',
+  'section-11-12',
+  'section-13-14',
+  'section-15-over'
+].forEach(sectionId => {
+  linkAllButtonsAgeSection(sectionId);
+});
