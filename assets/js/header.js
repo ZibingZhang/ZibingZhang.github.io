430c8b1bf3165c45e6196f2b19cf99573ef04a47
Zibing Zhang <zibing.zha@gmail.com>
2021-01-19 15:54:38 -0500

    removed unused code

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,5 @@
 window.onload = () => {
   let lists = Array.from(document.getElementsByTagName('LI'));
-  let link;
   for (let list of lists) {
     if (list?.firstElementChild?.href === 'https://zibingzhang.com/') {
       list.firstElementChild.href = 'https://zibingzhang.com/about/';
